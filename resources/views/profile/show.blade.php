bfd1d845bc44535ade2654b281679ae42f78690f
Edward Feliciano <felicianoec05@gmail.com>
2025-05-23 22:36:14 +0800

    *Update

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -8,6 +8,8 @@
                 <h5>Profile Information</h5>
                 <p><strong>Name:</strong> {{ $user->firstname }} {{ $user->lastname }}</p>
                 <p><strong>Email:</strong> {{ $user->email }}</p>
+                <p><strong>Phone:</strong> {{ $user->phone ?? 'N/A' }}</p>
+                <p><strong>Date of Birth:</strong> {{ optional($user->date_of_birth)->format('M d, Y') ?? 'N/A' }}</p>
                 <p><strong>Member since:</strong> {{ $user->created_at->format('M d, Y') }}</p>
                 
                 <a href="{{ route('dashboard') }}" class="btn btn-secondary">Back to Dashboard</a>
